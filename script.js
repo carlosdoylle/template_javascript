57b4549dff64655be029cb9102b10910320842d2
carlos <junior.pessoaagradavel@gmail.com>
2023-04-26 19:51:06 -0300

    exercicios feitos em aula

---
+++ script.js
@@ -0,0 +1,80 @@
+/* function minhaFuncao(variavel) {
+	return variavel * 5
+}
+
+console.log(minhaFuncao(2))
+console.log(minhaFuncao(10)) */
+
+/* let textoDoUsuario = prompt("Insira um texto");
+
+const outraFuncao = function(texto) {
+	return texto.toLowerCase().includes("cenoura")
+}
+
+const resposta = outraFuncao(textoDoUsuario)
+console.log(resposta) */
+
+/* function dados(){
+    console.log("Eu sou Carlos, tenho 17 anos, moro em São leopoldo e sou estudante")
+}
+
+dados() */
+
+/* let nome = prompt("digite seu nome")
+let idade = Number(prompt("digite sua idade"))
+let cidade = prompt("qual a sua cidade")
+let prefissao= prompt("qual sua profissao?")
+
+function info(dados){
+    return "eu sou "+nome+", tenho "+idade+" anos, moro em "+cidade+" e sou "+prefissao
+}
+
+ const mensagem = info(dados)
+console.log(mensagem) */
+
+//a) desafio
+
+/* function Exibirmensagem(texto){
+    return texto.toUpperCase() + ' - ' + texto.length
+}
+
+console.log(exibirMensagem('vamos gremio')) */
+
+//b) desafio
+
+/* function soma (n1,n2){
+    return n1 + n2;
+}
+
+function subtraçao (n1,n2){
+    return n1 - n2;
+}
+
+
+function divisao (n1,n2){
+    return n1 / n2;
+}
+
+function multiplicaçao (n1,n2){
+    return n1 * n2;
+}
+
+let somaDois = soma(2,3)
+let subDois = subtraçao(2,3)
+let divDois = divisao(2,3)
+let multDois = multiplicaçao(2,3)
+
+console.log('numeros insiridos '+2+','+3)
+console.log('soma '+ somaDois)
+console.log('diferença '+ subDois)
+console.log('divisao '+divDois)
+console.log('multiplicaçao '+multDois) */
+
+
+// fazer pitagoras
+
+/* function hipo(cat1, cat2){
+    return Math.sqrt(Math.pow(cat1,2) + Math.pow(cat2,2))
+}
+console.log(hipo(3,4).toFixed(2)) */
+
